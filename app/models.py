0ec5650309a460a2fb0f13816d50eae7bbcd4a98
huokr <docdedi@gmail.com>
2013-04-18 18:26:12 +0000

    m

--- app/models.py
+++ app/models.py
@@ -25,4 +25,4 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id')
 
     def __repr__(self):
-        return '<Post %r'> % (self.body)
+        return '<Post %r>' % (self.body)
