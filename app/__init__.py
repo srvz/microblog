747e1944417ba5c6aa3e12349b8dbbf58e4dbea6
huokr <docdedi@gmail.com>
2013-04-20 20:49:39 +0000

    fix ImportError

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,17 @@
-
+import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config.from_object('config')
-db = SQLAlchemy(app)
-
-from app import views, models
-
-import os
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
 from config import basedir
 
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
 lm = LoginManager()
-lm.setup_app(app)
+lm.init_app(app)
 lm.login_view = 'login'
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+from app import views, models
+
