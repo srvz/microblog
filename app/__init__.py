9269f98b9b0da731e8c8f24012f06c9c1ab0bb76
huokr <docdedi@gmail.com>
2013-04-20 15:06:10 +0000

    m

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,4 @@
 
-
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
@@ -8,3 +7,12 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 
 from app import views, models
+
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
+
+lm = LoginManager()
+lm.setup_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
